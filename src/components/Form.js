c13ba01495794d8a0a8f36138df923e2e9110aea
Sani Rogers <sanirogers138@gmail.com>
2023-01-31 10:09:09 -0500

    done react form submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  // eslint-disable-next-line
+  const [errors, setErrors] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,8 +14,21 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(e){
+    e.preventDefault();
+    const formData = {firstName: firstName, lastName: lastName};
+    if (firstName.length > 0 ){
+      props.sendFormData(formData);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"])
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
